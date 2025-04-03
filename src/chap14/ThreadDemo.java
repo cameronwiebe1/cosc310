67c8c072df506081f910eab75e401682e33e0dc4
Brian Toone <brtoone@samford.edu>
2025-04-03 14:01:16 -0500

    multi-threaded search

--- src/chap14/ThreadDemo.java
+++ src/chap14/ThreadDemo.java
@@ -1,34 +1,83 @@
 package chap14;
 
+import java.util.Random;
+
 public class ThreadDemo {
+    
+    static int[] nums = new int[100_000_000]; // 10 million elements
     public static void main(String[] args) {
-        Thread t1 = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    while(true) {
-                        Thread.sleep(1);
-                        System.out.println("hello thread1");
-                    }
-                } catch(InterruptedException ex) {
+        initNums();
 
-                }
+        // Search for a number in the array
+        long start = System.nanoTime();
+        int needle = 100_001; // number to search for ... won't be found ... maxium amount of time
+        int index = Searcher.findFirst(needle, nums);
+        if (index != -1) {
+            System.out.println("Found " + needle + " at index " + index);
+        } else {
+            System.out.println(needle + " not found");
+        }
+        long end = System.nanoTime();
+        System.out.println("Time taken to search for " + needle + ": " + (end - start)/1000 + " µs");
+
+        // Now let's try to search for the same number using multiple threads
+        start = System.nanoTime();
+        int nthreads = 3;
+
+        // split the search space into 10 different threads
+        int chunk = nums.length / nthreads;
+        SearchWorker workers[] = new SearchWorker[nthreads];
+        for (int i=0; i<nthreads; i++) {
+            int startIndex = i * chunk;
+            int endIndex = (i+1) * chunk;
+            if (i == nthreads - 1) {
+                endIndex = nums.length; // last thread takes the rest
             }
-        });
+            workers[i] = new SearchWorker(startIndex, endIndex, needle, nums);
+        }
 
-        Thread t2 = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    while(true) {
-                        Thread.sleep(1);
-                        System.out.println("goodbye thread2");
-                    }
-                } catch(InterruptedException ex) {
-                    
-                }
+        // start the threads
+        Thread threads[] = new Thread[nthreads];
+        for (int i=0; i<nthreads; i++) {
+            threads[i] = new Thread(workers[i]);
+            threads[i].start();
+        }
+        // wait for the threads to finish
+        for (int i=0; i<nthreads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
+        }
 
-        t1.start();
-        t2.start();
+        // check the results
+        int result = -1;
+        for (int i=0; i<nthreads; i++) {
+            int r = workers[i].getResult();
+            if (r != -1) {
+                result = r;
+                break;
+            }
+        }
+
+        end = System.nanoTime();
+        
+        if (result != -1) {
+            System.out.println("Found " + needle + " at index " + result);
+        } else {
+            System.out.println(needle + " not found");
+        }
+        System.out.println("Time taken to search for " + needle + " using threads: " + (end - start)/1000 + " µs");
+    }
+
+    public static void initNums() {
+        long start = System.currentTimeMillis();
+        Random rand = new Random();
+        for (int i=0; i<nums.length; i++) {
+            nums[i] = rand.nextInt(200_000) - 100_000; // random number between -100,000 and 100,000
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken to initialize nums: " + (end - start) + " ms");        
     }
 }
